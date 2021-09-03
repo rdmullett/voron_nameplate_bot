af57f3075805f9ec5943f7468126b8e8af7b1792
rmullett <rdmullett@gmail.com>
2021-09-03 11:20:33 -0700

    Add ability to parse for V0 machines and generate differnent nameplates for those serials

--- voron_nameplates/redditbot.py
+++ voron_nameplates/redditbot.py
@@ -8,6 +8,7 @@ import time
 #from voron_nameplates import googledrive
 import googledrive
 import subprocess
+import argparse
 
 clientID = os.environ["VORON_NAMEPLATE_ID"]
 clientSecret = os.environ["VORON_NAMEPLATE_SECRET"]
@@ -23,11 +24,17 @@ reddit = praw.Reddit(client_id=clientID, client_secret=clientSecret, password=cl
 
 registryuser = reddit.redditor("voron_registry_bot")
 
-def serial_grab_reddit():
-    if not os.path.isfile("/nameplates/logs/post_logs.txt"):
+def parse_args():
+    parser = argparse.ArgumentParser("This is a reddit bot that generates a nameplate for every new Voron serial number on /r/voroncorexy and provide it via a comment.")
+    parser.add_argument("-d", "--dry-run", help='Dry run to test functionality and avoid uploading to Google or making a comment on Reddit.')
+    args = parser.parse_args()
+    return args
+
+def serial_grab_reddit(runpath):
+    if not os.path.isfile(runpath + "logs/post_logs.txt"):
         postsRepliedTo=[]
     else:
-        with open("/nameplates/logs/post_logs.txt", "r") as f:
+        with open(runpath + "logs/post_logs.txt", "r") as f:
             postsRepliedTo = f.read()
             postsRepliedTo = postsRepliedTo.split("\n")
             postsRepliedTo = list(filter(None, postsRepliedTo))
@@ -51,11 +58,11 @@ def serial_grab_reddit():
             continue
     return serials, redditURLS
 
-def scad_create(serialList):
+def scad_create(serialList, runpath):
     for serial in serialList:
         #For some reason calling the openscad directly from python makes openscad very unhappy. Calling from bash script for now
-        if not os.path.isfile("/nameplates/" + serial + ".stl"):
-            subprocess.run(["scadcall.sh", serial])
+        if not os.path.isfile(runpath + serial + ".stl"):
+            subprocess.run(["scadcall.sh", serial, runpath])
         else:
             continue
 
@@ -63,7 +70,6 @@ def comment_create(googDict, redDict):
     for key, value in googDict.items():
         reddit_post = redDict[key][0]
         googleURL = googDict[key][0]
-#TODO: remove the following comments when ready to post again
         comment = "Congratulations on your serial! I generated a nameplate for your build that you can download here:\n\n" + googleURL + "\n\nIf there are issues with this bot please contact /u/iDuumb."
         print(comment)
         with open("/nameplates/logs/post_logs.txt", "a") as f:
@@ -73,13 +79,25 @@ def comment_create(googDict, redDict):
         submission.reply(comment)
         time.sleep(610)
 
-def main():
-    serials, redditURLS = serial_grab_reddit()
-    scad_create(serials)
-    service = googledrive.service()
+def dry_run():
+    runpath = "/dry-run/"
+    serials, redditURLS = serial_grab_reddit(runpath)
+    scad_create(serials, runpath)
+    
+def production_run():
+    runpath = "/nameplates/"
+    serials, redditURLS = serial_grab_reddit(runpath)
+    scad_create(serials, runpath)
     googledrive.serial_folder_create(service, serials)
     googURLS = googledrive.serial_stl_upload(service, serials)
-    comment_create(googURLS, redditURLS) 
+    comment_create(googURLS, redditURLS)
+
+def main():
+    options = parse_args()
+    if options.dry-run():
+       dry_run()
+    else:
+        production_run()
 
 if __name__ == '__main__':
     main()
--- voron_nameplates/scadcall.sh
+++ voron_nameplates/scadcall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-openscad -o "/nameplates/"$1"-NoLogo.stl" -D 'serial="'$1'"' -D "logo=false" "/voron_serial_plate/Voron_Logo_Plate.scad"
-openscad -o "/nameplates/"$1".stl" -D 'serial="'$1'"' "/voron_serial_plate/Voron_Logo_Plate.scad"
+if [[ $1 == V0* ]] ;
+then	
+openscad -o $2$1"-NoLogo.stl" -D 'serial="'$1'"' -D "logo=false" "/voron_serial_plate/V0/Voron0_Logo_Plate.scad"
+openscad -o $2$1".stl" -D 'serial="'$1'"' "/voron_serial_plate/V0/Voron0_Logo_Plate.scad"
+else
+openscad -o $2"$1"-NoLogo.stl" -D 'serial="'$1'"' -D "logo=false" "/voron_serial_plate/Voron_Logo_Plate.scad"
+openscad -o $2"$1".stl" -D 'serial="'$1'"' "/voron_serial_plate/Voron_Logo_Plate.scad"
+fi
