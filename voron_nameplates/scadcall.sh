995319f2895c8503da5d612955c99fe1b93e7d45
Ryan Mullett <rdmullett@gmail.com>
2021-09-06 12:32:02 -0700

    Fix syntax issue within scadcall.sh

--- voron_nameplates/scadcall.sh
+++ voron_nameplates/scadcall.sh
@@ -5,6 +5,6 @@ then
 openscad -o $2$1"-NoLogo.stl" -D 'serial="'$1'"' -D "logo=false" "/voron_serial_plate/V0/Voron0_Logo_Plate.scad"
 openscad -o $2$1".stl" -D 'serial="'$1'"' "/voron_serial_plate/V0/Voron0_Logo_Plate.scad"
 else
-openscad -o $2"$1"-NoLogo.stl" -D 'serial="'$1'"' -D "logo=false" "/voron_serial_plate/Voron_Logo_Plate.scad"
-openscad -o $2"$1".stl" -D 'serial="'$1'"' "/voron_serial_plate/Voron_Logo_Plate.scad"
+openscad -o $2$1"-NoLogo.stl" -D 'serial="'$1'"' -D "logo=false" "/voron_serial_plate/Voron_Logo_Plate.scad"
+openscad -o $2$1".stl" -D 'serial="'$1'"' "/voron_serial_plate/Voron_Logo_Plate.scad"
 fi
